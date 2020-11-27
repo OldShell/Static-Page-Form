f4eb436cf2bd65f48ab946ad4d95116e5c85aece
OldShell <44648811+OldShell@users.noreply.github.com>
2020-11-27 13:46:23 +0000

    Update formD.js

--- formD.js
+++ formD.js
@@ -13,7 +13,7 @@ function initForm(){
 function fromLocalFile(buttonId,form){
 	var str = "input[id=" + buttonId + "]"; // id of input button
 	var file = document.querySelector(str).files[0];
-	if(inFilename = "no_change"){
+	if(inFilename == "no_change"){
 		inFilename = file.name;
 	}
 	inFilename = inFilename.replace(/\.[^/.]+$/, "");
